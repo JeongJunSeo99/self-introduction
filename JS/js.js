c0ad1d93cd5c1eefccb6e26b60f2c86b08f16952
wnstj <wnstj0810@naver.com>
2022-05-04 10:47:43 +0900

    commit 8

---
+++ JS/js.js
@@ -0,0 +1,29 @@
+/* 
+    Reference
+    https://github.com/chanho-Yoon/self-introduction-web/js
+    License: none (public domain)
+*/
+
+const navHomeClickEvent = document.getElementById('nav-home');
+const navAboutClickEvent = document.getElementById('nav-about');
+const navProjectClickEvent = document.getElementById('nav-project');
+const navFooterClickEvent = document.getElementById('nav-footer');
+
+navHomeClickEvent.addEventListener('click', () => {
+	window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+navAboutClickEvent.addEventListener('click', () => {
+	let offset = Number(document.getElementById('my-name').offsetTop);
+	window.scrollTo({ top: offset - 40, behavior: 'smooth' });
+});
+
+navProjectClickEvent.addEventListener('click', () => {
+	let offset = Number(document.getElementById('my-project').offsetTop);
+	window.scrollTo({ top: offset - 40, behavior: 'smooth' });
+});
+
+navFooterClickEvent.addEventListener('click', () => {
+	let offset = Number(document.getElementById('footer').offsetTop);
+	window.scrollTo({ top: offset - 40, behavior: 'smooth' });
+});
